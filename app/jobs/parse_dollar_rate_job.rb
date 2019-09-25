217d4c1c5d7e9c30b20b18af82760cd98b198f2c
fukk.u.3kpvc <ykwnd@yahoo.com>
2019-09-25 17:05:48 +0500

    parse usd rate from cbr

--- app/jobs/parse_dollar_rate_job.rb
+++ app/jobs/parse_dollar_rate_job.rb
@@ -1,7 +1,25 @@
+require 'open-uri'
+
 class ParseDollarRateJob < ApplicationJob
   queue_as :default
+  attr_accessor :cbr, :usd_xpath, :usd_rate
 
   def perform(*args)
-    # Do something later
+    initialize_vars
+    parse_usd_rate
+  end
+
+  private
+
+  def initialize_vars
+    @cbr = 'https://www.cbr.ru/currency_base/daily/'
+    @usd_xpath = '//*[@id="content"]/table/tbody/tr[12]/td[5]'
+  end
+
+  def parse_usd_rate
+    cbr_page = open(@cbr)
+    document = Nokogiri::HTML(cbr_page)
+    @usd_rate = document.xpath(@usd_xpath)
+    puts @usd_rate.text
   end
 end
