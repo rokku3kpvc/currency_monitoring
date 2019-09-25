3c7051a044a1e7491060268e5343cc8aa300181c
fukk.u.3kpvc <ykwnd@yahoo.com>
2019-09-25 18:11:58 +0500

    routes and controllers form

--- app/controllers/currency_controller.rb
+++ app/controllers/currency_controller.rb
@@ -1,5 +1,14 @@
 class CurrencyController < ApplicationController
+  http_basic_authenticate_with name: 'admin', password: 'admin', except: :index
+
   def index
+  end
+
+  def new
+
+  end
+
+  def create
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'currency#index'
+  get 'admin', to: 'currency#new'
+  post 'currency', to: 'currency#create'
 
   mount ActionCable.server => '/cable'
 end
