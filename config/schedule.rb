d5c34a7baba07816f0e7428014cdb61dd4d6740b
fukk.u.3kpvc <ykwnd@yahoo.com>
2019-09-25 21:36:34 +0500

    schedule parse dollar rate job

---
+++ config/schedule.rb
@@ -0,0 +1,7 @@
+set :output, "#{path}/log/cron_jobs.log"
+set :environment, 'development'
+job_type :runner_with_rvm, 'source /etc/profile.d/rvm.sh; cd :path;rvm 2.5.1@currency_monitoring do bundle exec rails runner -e :environment :task  :output'
+
+every 1.minute do
+  runner_with_rvm "ParseDollarRateJob.perform_later"
+end
